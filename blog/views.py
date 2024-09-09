a10ad2d23cd772e31b3b44bb3ccdc27152f3c3ef
laijinwei <18776666967@qq.com>
2024-09-09 18:05:14 +0800

    feature: add template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter().order_by('published_time').all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
