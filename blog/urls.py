bbf2a46ed11a84901402b8ef33f7a3c66bcb1d3c
laijinwei <18776666967@qq.com>
2024-09-27 10:15:41 +0800

    feat: Added golden points.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post_detail/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
+    path('golden_point/', views.golden_point, name='golden_point'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
@@ -52,3 +54,32 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def golden_point(request):
+    if request.method == 'POST':
+        num1 = float(request.POST['num1'])
+        num2 = float(request.POST['num2'])
+
+        collect = []
+        base_points = [0, 0.168, 0.382, 0.5, 0.618, 0.736, 1]
+
+        if num1 < num2:
+            # base_points = [-i for i in base_points]
+            pass
+        else:
+            base_points.extend([1.168, 1.382, 1.5, 1.618, 1.736, 2])
+
+        top = max(num1, num2)
+        bottom = min(num1, num2)
+
+        for point in base_points:
+            number = (top - bottom) * point + bottom
+            collect.append(round(number, 2))
+
+        result = json.dumps(dict(zip(base_points, collect)), indent=4)
+
+        return render(request, 'blog/golden_point.html', {'points': result})
+        pass
+    else:
+        return render(request, 'blog/golden_point.html')
